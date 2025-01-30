73500bab1f355db4fcbf6dabf7aea9729a294016
Joren Frederickx <joren.frederickx@student.ehb.be>
2025-01-30 19:45:01 +0100

    mobiel homepage

--- website.js
+++ website.js
@@ -90,3 +90,14 @@ window.addEventListener("scroll", function () {
 		nav.classList.remove("scrolled");
 	}
 });
+
+
+
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
